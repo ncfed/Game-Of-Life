08ff696be1d8ce4464ddfba5ad58b731a5634b7c
Dm Fed <fe.off@mail.ru>
2017-02-25 01:51:53 +0400

    ready

---
+++ src/gol/GOL.java
@@ -0,0 +1,14 @@
+package gol;
+
+public class GOL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameOfLife life = new GameOfLife();
+        life.setLocationRelativeTo(null);
+        life.show();
+    }
+    
+}
